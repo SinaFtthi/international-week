60dd977ceff04037e6fb27ac2df230c1a84fdd0d
Aiche-cmr <aiche.camara@student.odisee.be>
2026-04-28 10:42:12 +0200

    Api design

--- src/server/index.js
+++ src/server/index.js
@@ -9,8 +9,54 @@ app.get('/', (req, res) => {
   res.json({ message: 'Server is running!' });
 });
 
+// Payment Orders
+app.post('/payment-orders', (req, res) => {
+  res.json({ message: 'Create PO endpoint' });
+});
 
+app.get('/payment-orders', (req, res) => {
+  res.json({ message: 'Get all POs' });
+});
 
+app.post('/payment-orders/:id/validate', (req, res) => {
+  res.json({ message: 'Validate PO' });
+});
+
+app.post('/payment-orders/:id/send', (req, res) => {
+  res.json({ message: 'Send PO to CB' });
+});
+
+// ACK
+app.get('/acknowledgements', (req, res) => {
+  res.json({ message: 'Get ACKs' });
+});
+
+app.post('/acknowledgements/:id/process', (req, res) => {
+  res.json({ message: 'Process ACK' });
+});
+
+// BB (incoming)
+app.post('/po_in', (req, res) => {
+  res.json({ message: 'Receive PO from CB' });
+});
+
+app.post('/ack_out', (req, res) => {
+  res.json({ message: 'Send ACK to CB' });
+});
+
+// Accounts
+app.get('/accounts', (req, res) => {
+  res.json({ message: 'Get accounts' });
+});
+
+app.get('/accounts/:id', (req, res) => {
+  res.json({ message: 'Get account by ID' });
+});
+
+// Transactions
+app.get('/transactions', (req, res) => {
+  res.json({ message: 'Get transactions' });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
